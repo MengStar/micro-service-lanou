f2b85a9d99de1a878e2c863d31d03fab7bd024c3
MengStar <641510128@qq.com>
2017-08-29 23:19:19 +0800

    新增配置 服务

--- service-auth/src/main/java/meng/xing/HelloController.java
+++ service-auth/src/main/java/meng/xing/HelloController.java
@@ -1,12 +1,18 @@
 package meng.xing;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class HelloController {
+    @Value("${returnString}")
+    private String ret;
+
     @GetMapping("/hello")
     public String hello() {
-        return "service-auth hello";
+        return "service-auth hello" + this.ret;
     }
 }
