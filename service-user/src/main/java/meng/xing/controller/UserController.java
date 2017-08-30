b397bbb476a85909074a2cbc176a0ac44e28c9b8
MengStar <641510128@qq.com>
2017-08-30 23:42:00 +0800

    去除多余日志

--- service-user/src/main/java/meng/xing/controller/UserController.java
+++ service-user/src/main/java/meng/xing/controller/UserController.java
@@ -39,9 +39,7 @@ public class UserController {
     public Page<User> getUsers(@RequestParam(value = "page", defaultValue = "1") Integer page,
                                @RequestParam(value = "pageSize", defaultValue = "10") Integer pageSize,
                                @RequestParam(value = "sort", defaultValue = "id") String sort,
-                               @RequestParam(value = "order", defaultValue = "asc") String order,
-                               @RequestParam(value = "username", required = false) String username) {
-        System.out.println(username);
+                               @RequestParam(value = "order", defaultValue = "asc") String order) {
         Sort _sort = new Sort(Sort.Direction.fromString(order), sort);
         //传来的页码是从1开始，而服务器从1开始算
         Pageable pageable = new PageRequest(page - 1, pageSize, _sort);
