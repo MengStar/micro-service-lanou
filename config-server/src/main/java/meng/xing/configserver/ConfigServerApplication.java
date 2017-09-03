3370263493d5d0c0adf21da359bb9b91923ff8c4
MengStar <641510128@qq.com>
2017-09-03 10:16:55 +0800

    简化启动注解

--- config-server/src/main/java/meng/xing/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/meng/xing/configserver/ConfigServerApplication.java
@@ -1,16 +1,14 @@
 package meng.xing.configserver;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-@SpringBootApplication
+@SpringCloudApplication
 @EnableConfigServer
-@EnableDiscoveryClient
 public class ConfigServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConfigServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
 }
