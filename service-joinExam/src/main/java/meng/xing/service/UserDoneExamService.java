95e9584403c016590ec8f2a3264f99ff16ed6dbe
MengStar <641510128@qq.com>
2018-03-06 12:26:36 +0800

    修改项目结构

--- service-joinExam/src/main/java/meng/xing/service/UserDoneExamService.java
+++
@@ -1,9 +0,0 @@
-package meng.xing.service;
-
-import meng.xing.entity.UserDoneExam;
-
-public interface UserDoneExamService {
-    boolean add(UserDoneExam userDoneExam);
-
-    boolean isExist(String username, Long examId);
-}
